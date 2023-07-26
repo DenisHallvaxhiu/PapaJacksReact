2f792211a80a51688f85e8ad86b096683da6e323
Denis Hallvaxhiu <dhallvaxhiu999@gmail.com>
2023-07-25 20:35:28 -0400

    Adding booking api

---
+++ src/Components/BookingForm/BookingForm.jsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import "./BookingForm.css";
+
+// Seeded random function
+const seededRandom = (seed) => {
+  const m = 2 ** 35 - 31;
+  const a = 185852;
+  let s = seed % m;
+
+  return () => (s = (s * a) % m) / m;
+};
+
+// Fetch API function
+const fetchAPI = (date) => {
+  let result = [];
+  let random = seededRandom(date.getDate());
+
+  for (let i = 17; i <= 23; i++) {
+    if (random() < 0.5) result.push(i + ":00");
+    if (random() < 0.5) result.push(i + ":30");
+  }
+
+  return result;
+};
+
+export default function Booking() {
+  const [availableTimes, setAvailableTimes] = useState([
+    "10:00 AM",
+    "12:00 PM",
+    "02:00 PM",
+    "06:00 PM",
+  ]);
+
+  const [selectedTime, setSelectedTime] = useState("");
+
+  function handleTimeChange(selectedTime) {
+    setSelectedTime(selectedTime);
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    if (selectedTime) {
+      // Filter out the selected time and update the availableTimes state
+      setAvailableTimes((prevTimes) =>
+        prevTimes.filter((time) => time !== selectedTime)
+      );
+
+      // Reset the selectedTime state to clear the selection
+      setSelectedTime("");
+    }
+  }
+
+  useEffect(() => {
+    initializeTimes(); // Call the function to fetch initial times when the component mounts
+  }, []);
+
+  const initializeTimes = () => {
+    const today = new Date();
+    const initialTimes = fetchAPI(today);
+    setAvailableTimes(initialTimes);
+  };
+
+  return (
+    <div className="formContainer">
+      <form>
+        <div>
+          <label htmlFor="res-time">Choose time</label>
+          <select
+            onChange={(e) => handleTimeChange(e.target.value)}
+            id="res-time"
+          >
+            {availableTimes.length > 0 ? (
+              <option>Select a time</option>
+            ) : (
+              <option>No available times!</option>
+            )}
+            {availableTimes.map((value, index) => (
+              <option key={index} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="guests">Number of guests</label>
+          <input
+            type="number"
+            placeholder="1"
+            min="1"
+            max="10"
+            id="guests"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="occasion">Occasion</label>
+          <select id="occasion">
+            <option>Birthday</option>
+            <option>Anniversary</option>
+            <option>Other</option>
+          </select>
+        </div>
+        <input
+          type="submit"
+          onClick={handleFormSubmit}
+          value="Make Your reservation"
+        ></input>
+      </form>
+    </div>
+  );
+}
--- src/views/Booking.jsx
+++ src/views/Booking.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import BookingForm from "../Components/BookingForm/BookingForm"
+import image from "../assets/wood-background.jpg"
 
 export default function Booking() {
+  const divStyle = {
+    background: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 10%, rgba(0, 0, 0, 1)), url(${image})`,
+    height: '93vh',
+    overflow: 'hidden',
+  };
   return (
-    <div>Booking</div>
-  )
+    <div style={divStyle}>
+      <BookingForm/>
+    </div>
+  );
 }
